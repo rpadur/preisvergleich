24c1de6b4a806111265b294456bfc9d0be7a61e5
Ronald Padur <ronald.padur@dxmccw1.tolina.local>
2015-08-25 19:12:52 +0200

    Umstellung auf Angular-HAL

--- app/produkte/controllers.js
+++ app/produkte/controllers.js
@@ -1,33 +1,52 @@
-var app = angular.module("preisvergleichApp.controllers", []);
+var app = angular.module("preisvergleichApp.controllers", [ 'angular-hal' ]);
 app.controller('ProduktCtrl', function($scope, $resource, ProduktService) {
+    $scope.articles = [];
+    $scope.neuesProdukt = {
+	name : '',
+	preis : ''
+    };
     $scope.orderProp = 'name';
-    $scope.neuesProdukt = new ProduktService({});
-    // Laden aller Produkte
-    $scope.articles = ProduktService.query(function() {
-	console.log($scope.articles);
-    });
+    var setup = function(pagenumber) {
+
+	ProduktService.load().then(function(websiteResoure) {
+	    return websiteResoure.$get('produkte', {
+		'page' : pagenumber,
+		'size' : 10,
+		'sort' : null
+	    }).then(function(resource) {
+		$scope.page = resource.page;
+		$scope.page.currentPage = $scope.page.number + 1;
+		return resource.$get('produkte');
+	    }).then(function(produktListe) {
+		$scope.articles = produktListe;
+	    });
+	});
+
+    };
+
+    setup(0);
 
     $scope.saveProdukt = function(name, preis) {
 	console.log(name);
 	console.log(preis);
-	
-	
-	
-	$scope.neuesProdukt.$save();
+	ProduktService.load().then(function(resource) {
+	    return resource.$post('produkte', {}, $scope.neuesProdukt);
+	})
 	$scope.articles.push({
+	    name : $scope.neuesProdukt.name,
+	    preis : $scope.neuesProdukt.preis
+	})
+	$scope.neuesProdukt = {
+	    name : '',
+	    preis : ''
+	};
 
-	    name : name,
-	    preis : preis,
-	    id: $scope.neuesProdukt.id
-	});
-	$scope.neuesProdukt.name = "";
-	$scope.neuesProdukt.preis = "";
-	$scope.neuesProdukt.id="";
     };
 
     $scope.deleteProdukt = function(/** Integer */
     idx) {
-
+	var produkt = $scope.articles[idx];
+	produkt.$del('self');
 	$scope.articles.splice(idx, 1);
     };
 
@@ -41,5 +60,7 @@ app.controller('ProduktCtrl', function($scope, $resource, ProduktService) {
 app.controller('ProduktDetailCtrl', function($scope, $routeParams,
 	ProduktService) {
     $scope.produktId = $routeParams.produktId;
-    $scope.produkt = ProduktService.get({id: $scope.produktId});
+    $scope.produkt = ProduktService.get({
+	id : $scope.produktId
+    });
 })
--- app/produkte/services.js
+++ app/produkte/services.js
@@ -1,13 +1,26 @@
-var app = angular.module("preisvergleichApp.services", [])
-app.factory("ProduktService", function($resource) {
+    
+'use strict';
 
-     return $resource('http://localhost:8080/produkte/:id', {
-     id : '@_id'
-     }, {
-     });
+var app = angular.module("preisvergleichApp.services", ['angular-hal'])
+app.factory("ProduktService", ['halClient', function(halClient) {
+
+     return {
+	 'load' :
+	     function() {
+	     return halClient.$get('http://localhost:8080/');
+	     }
+     }
+     
+//     $resource('http://localhost:8080/produkte/:id', {
+//     id : '@_id'
+//     }, {
+//     });
 
 //    return $resource(
 //	    'http://109.230.233.225:8080/PreisvergleichServer/produkte/:id', {
 //		id : '@_id'
 //	    }, {});
-});
+}]);
+
+
+
